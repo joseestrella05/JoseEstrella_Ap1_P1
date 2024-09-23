af31a568b537c8eb8e2b53b98cca7b8d0048266f
jose gabriel <jose gabriel@JOSEGABRIEL>
2024-09-23 19:04:42 -0400

    contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using JoseEstrella_Ap1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JoseEstrella_Ap1_P1.DAL;
+
+public class Contexto(DbContextOptions<Contexto> options): DbContext(options)
+{
+
+    public DbSet<Registro> Registro { get; set; }
+}
---
+++ Models/Registro.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JoseEstrella_Ap1_P1.Models;
+
+public class Registro
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Nombre { get; set; }
+
+}
