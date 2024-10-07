0fd0839cd840b68972cee0ff3a2730c46a6d3a7f
jose gabriel <jose gabriel@JOSEGABRIEL>
2024-10-07 12:25:35 -0400

    Agregar expresiones regulares

--- Models/Pretamos.cs
+++ Models/Pretamos.cs
@@ -8,12 +8,15 @@ public class Pretamos
     public int Id { get; set; }
    
     [Required(ErrorMessage = "el campo deudor es obligatiorio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "No se permiten Numero")]
     public string Deudor { get; set; }
 
     [Required(ErrorMessage = "campo obligatotio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "No se permiten Numero")]
     public string Concepto { get; set; }
 
     [Required(ErrorMessage = "El monto es obligatorio")]
+    [Range(0, 200000, ErrorMessage = "Favor de introducir un sueldo mayor que 1 y menor que 200000.")]
     public int Monto { get; set; }
 
 }
