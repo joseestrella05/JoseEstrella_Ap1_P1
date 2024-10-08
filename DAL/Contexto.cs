4784ee0f95baeed92565a751fab8fe8c3f07b7b9
jose gabriel <jose gabriel@JOSEGABRIEL>
2024-10-08 17:19:33 -0400

    OnModelCreating en el contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -7,4 +7,13 @@ public class Contexto(DbContextOptions<Contexto> options): DbContext(options)
 {
 
     public DbSet<Pretamos> pretamos { get; set; }
+    public DbSet<Deudores> Deudores { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Deudores>().HasData(new List<Deudores>(){
+            new Deudores() { DeudorId=1, Nombres="Reyphil"}
+        });
+    }
 }
