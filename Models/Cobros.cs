f06d2aaa45e819aae71262892a4f086551235ce6
jose gabriel <jose gabriel@JOSEGABRIEL>
2024-10-11 21:02:24 -0400

    creacion del modelo cobro

---
+++ Models/Cobros.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JoseEstrella_Ap1_P1.Models
+{
+    public class Cobros
+    {
+        [Key]
+        public int cobroId { get; set; }
+        [Required(ErrorMessage = "fecha obligatoria")]
+        public DateTime Fecha {  get; set; }
+        [ForeignKey("Deudores")]
+        [Required(ErrorMessage = "Selecionar un Deudor")]
+        public int DeudorId { get; set; }
+
+        [ForeignKey("DeudorId")]
+        public Deudores Deudores { get; set; }
+
+        [ForeignKey("Prestamos")]
+        public int PrestamoId {  get; set; }
+
+        [ForeignKey("PrestamoId")]
+        public Pretamos pretamos { get; set; }
+
+    }
+}
