27b835bd7e5909b74d83a1b3f5c3460b12b4f1ed
jose gabriel <jose gabriel@JOSEGABRIEL>
2024-10-13 01:11:48 -0400

    Actulizacion de contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -6,14 +6,18 @@ namespace JoseEstrella_Ap1_P1.DAL;
 public class Contexto(DbContextOptions<Contexto> options): DbContext(options)
 {
 
-    public DbSet<Pretamos> pretamos { get; set; }
+    public DbSet<Prestamos> Prestamos { get; set; }
     public DbSet<Deudores> Deudores { get; set; }
+    public DbSet<Cobros> Cobros { get; set; }
+    public DbSet<CobrosDetalles> CobrosDetalles { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        modelBuilder.Entity<Deudores>().HasData(new List<Deudores>(){
-            new Deudores() { DeudorId=1, Nombres="Reyphil"}
+        modelBuilder.Entity<Deudores>().HasData(new List<Deudores>()
+        {
+            new Deudores() { DeudoresId = 1, Nombres = "Reyphill Nuñez" }
+
         });
     }
 }
